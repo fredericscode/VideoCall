db66f5bc088ec5f45f21a65ece689c55c6e9c345
Fredericscode <fredericscode@gmail.com>
2019-12-28 04:25:46 +0000

    Initial release

---
+++ db/migrate/20191228005703_add_details_to_users.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :avatar, :string
+    add_column :users, :level, :string
+    add_column :users, :github_link, :string
+    add_column :users, :state, :integer, default: 0
+  end
+end
